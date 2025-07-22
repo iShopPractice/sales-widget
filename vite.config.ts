e55cdda1f26206c1e08f6908042dffd3d148441f
JoaquinGonzalez <jgonzales@infosis.tech>
2025-07-22 13:18:44 -0300

    Version 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
+import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'sales_widget',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './SalesWidget': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
   },
 })
